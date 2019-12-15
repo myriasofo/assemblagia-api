dbc8b8e06b33b6021eeff9afd269e0d0fa46d1f3
Abe <myriasofo@gmail.com>
2019-12-15 16:31:48 -0500

    Upgrade tests

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/item', function(req, res, next) {
+  res.status(201).send()
+})
+
+router.put('/item', function(req, res, next) {
+  res.status(200).send()
+})
+
 router.get('/item', function(req, res, next) {
   res.status(200).send()
 })
+
+router.delete('/item', function(req, res, next) {
+  res.status(200).send()
+})
+
 
 module.exports = router;
--- tests/items.test.js
+++ tests/items.test.js
@@ -3,9 +3,35 @@ const request = require('supertest');
 const app = require('../app')
 
 describe('Test the root path', () => {
+  test('endpoint options', async () => {
+    const res = await request(app).options('/item')
+    expect(res.text).toEqual('POST,PUT,GET,HEAD,DELETE')
+  })
+
   test('It should response the GET method', async () => {
     const res = await request(app)
       .get('/item')
+
+    expect(res.statusCode).toEqual(200)
+  });
+
+  test('It should response the POST method', async () => {
+    const res = await request(app)
+      .post('/item')
+
+    expect(res.statusCode).toEqual(201)
+  });
+
+  test('It should response the PUT method', async () => {
+    const res = await request(app)
+      .put('/item')
+
+    expect(res.statusCode).toEqual(200)
+  });
+
+  test('It should response the DELETE method', async () => {
+    const res = await request(app)
+      .delete('/item')
 
     expect(res.statusCode).toEqual(200)
   });
